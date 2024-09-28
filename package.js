a9728fbc17db2f88841e735d30dab8296a580178
Kelly Copley <copleykj@gmail.com>
2024-09-28 18:34:42 -0400

    v3.0.0-beta.0

--- package.js
+++ package.js
@@ -10,11 +10,10 @@ Package.onUse(function (api) {
   api.versionsFrom('METEOR@3.0.1');
   api.use('webapp', 'server');
   api.use(['ejson', 'ecmascript', 'typescript'], ['server', 'client']);
-  api.use('zodern:types@1.0.13');
-  api.mainModule('lib/namespace.ts', ['server', 'client']);
+  api.mainModule('lib/namespace.js', ['server', 'client']);
   api.addFiles('lib/utils.js', ['server', 'client']);
   api.addFiles('lib/server.js', 'server');
-  api.addFiles('lib/client.ts', 'client');
+  api.addFiles('lib/client.js', 'client');
   api.export('InjectData', ['client', 'server']);
 });
 
@@ -23,8 +22,7 @@ Package.onTest(function (api) {
   api.use('webapp', 'server');
   api.use(['meteortesting:browser-tests@1.7.0', 'meteortesting:mocha@3.2.0']);
   api.use(['ecmascript', 'typescript', 'underscore'], ['client', 'server']);
-  api.use('zodern:types@1.0.13');
-  api.use(['http', 'random'], 'server');
+  api.use(['fetch', 'random'], 'server');
   api.use('communitypackages:picker@2.0.0-beta.0', 'server');
   api.mainModule('tests/server/index.js', 'server');
   api.mainModule('tests/client/index.js', 'client');
